b82db03652a374a0902bea8e3d5b6ddd16ad4a11
elecage <elecage@gmail.com>
2012-04-08 11:44:30 +0000

    Opr01 클래스 추가 - 수치데이터로 사칙연산

---
+++ JavaTest/src/elecage/fun_robot/test/Opr01.java
@@ -0,0 +1,23 @@
+package elecage.fun_robot.test;
+
+public class Opr01 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a = 10, b = 4, c;
+		c = a + b;
+		System.out.println( a + " + " + b +  " = " + c);
+		c = a - b;
+		System.out.println( a + " - " + b +  " = " + c);
+		c = a * b;
+		System.out.println( a + " * " + b +  " = " + c);
+		c = a / b;
+		System.out.println( a + " / " + b +  " = " + c);
+		c = a % b;
+		System.out.println( a + " % " + b +  " = " + c);
+	}
+
+}
