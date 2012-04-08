76383d929e7f623a9db28e7729de9824627919ba
elecage <elecage@gmail.com>
2012-04-08 11:14:41 +0000

    Data07, Data08 클래스 추가

---
+++ JavaTest/src/elecage/fun_robot/test/Data07.java
@@ -0,0 +1,21 @@
+package elecage.fun_robot.test;
+
+public class Data07 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		char x;
+		x = 'A';
+		System.out.printf("%c -> %d \n", x, (int)x);
+		x = '0';
+		System.out.printf("%c -> %d \n", x, (int)x);
+		x = 0;
+		System.out.printf("%c -> %d \n", x, (int)x);
+		x = 'a';
+		System.out.printf("%c -> %d \n", x, (int)x);
+	}
+
+}
