8bff7ecfe949310f9359a828a157d9e381a9abc2
elecage@gmail.com <elecage@gmail.com@6398e2c4-0f80-3df1-c908-689aad24dd59>
2012-04-08 11:00:35 +0000

    Data03 클래수 추가 - 정수 상수를 저장하는 정수형 변수

---
+++ trunk/JavaTest/src/elecage/fun_robot/test/Data03.java
@@ -0,0 +1,18 @@
+package elecage.fun_robot.test;
+
+public class Data03 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a;
+		a = 1;
+		System.out.println(a);
+		
+		a = 2;
+		System.out.println(a);
+	}
+
+}
