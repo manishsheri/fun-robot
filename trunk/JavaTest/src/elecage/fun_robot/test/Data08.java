d14b426c62387fcb066561d93d5ac2ae38e6b1e1
elecage@gmail.com <elecage@gmail.com@6398e2c4-0f80-3df1-c908-689aad24dd59>
2012-04-08 11:14:41 +0000

    Data07, Data08 클래스 추가

---
+++ trunk/JavaTest/src/elecage/fun_robot/test/Data08.java
@@ -0,0 +1,17 @@
+package elecage.fun_robot.test;
+
+public class Data08 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String y;
+		y = "AB";
+		System.out.println(y);
+		y = "A";
+		System.out.println(y);
+	}
+
+}
