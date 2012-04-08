32fa456181527e3a081f69c5a197be28250fd7d9
elecage <elecage@gmail.com>
2012-04-08 10:53:08 +0000

    자바에서 사용되는 상수의 종류

---
+++ JavaTest/src/elecage/fun_robot/test/Data01.java
@@ -0,0 +1,16 @@
+package elecage.fun_robot.test;
+
+public class Data01 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(1);
+		System.out.println(1.5);
+		System.out.println('a');
+		System.out.println(true);
+	}
+
+}
