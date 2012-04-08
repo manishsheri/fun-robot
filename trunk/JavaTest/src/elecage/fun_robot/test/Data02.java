e77de651d14eae5b6eba7bb78a055aa847d67f1d
elecage@gmail.com <elecage@gmail.com@6398e2c4-0f80-3df1-c908-689aad24dd59>
2012-04-08 10:57:54 +0000

    Data02 추가 - 더 다양한 상수

---
+++ trunk/JavaTest/src/elecage/fun_robot/test/Data02.java
@@ -0,0 +1,15 @@
+package elecage.fun_robot.test;
+
+public class Data02 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println(1L);
+		System.out.println(1.5f);
+		System.out.println("abc");
+	}
+
+}
