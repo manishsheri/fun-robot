94fdb473fb17d3d8b6ea4a7c2b15f1e2518ef8db
elecage <elecage@gmail.com>
2012-04-08 11:03:33 +0000

    Data05 클래스 추가 - 암시적인 형변환과 명시적인 형변환

---
+++ JavaTest/src/elecage/fun_robot/test/Data05.java
@@ -0,0 +1,21 @@
+package elecage.fun_robot.test;
+
+public class Data05 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		byte a = 1;
+		short b = 128;
+		int c = 32768;
+		
+		b = a;
+		System.out.println(b);
+		
+		b = (short) c;
+		System.out.println(b);
+	}
+
+}
