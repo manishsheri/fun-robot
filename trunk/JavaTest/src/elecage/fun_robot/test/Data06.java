1e342616822f3458a20bcdc6df225c4eef674442
elecage@gmail.com <elecage@gmail.com@6398e2c4-0f80-3df1-c908-689aad24dd59>
2012-04-08 11:06:29 +0000

    Data06 클래스 추가 - 실수형태의 자료형 저장하기

---
+++ trunk/JavaTest/src/elecage/fun_robot/test/Data06.java
@@ -0,0 +1,16 @@
+package elecage.fun_robot.test;
+
+public class Data06 {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		double a = 23.7;
+		float b = 23.7f;
+		System.out.println(a);
+		System.out.println(b);
+	}
+
+}
