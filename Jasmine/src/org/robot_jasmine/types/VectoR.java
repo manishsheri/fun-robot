5e0d179e4dd728eff832da56c25eafd2d651ef07
elecage <elecage@gmail.com>
2012-04-17 13:56:49 +0000

    Vector class is implemented. Matrix class is generated.

--- Jasmine/src/org/robot_jasmine/types/VectoR.java
+++ Jasmine/src/org/robot_jasmine/types/VectoR.java
@@ -37,16 +37,16 @@ public class VectoR implements Cloneable, java.io.Serializable{
 	protected int 		size;
 	protected double[] 	element;
 	
-	public VectoR(int size) {
-		super();
-		this.size = size;
-		this.element = new double[size];
-	}
-
 	public VectoR(double[] element) {
 		super();
 		this.size = element.length;
 		this.element = element;
+	}
+
+	public VectoR(int size) {
+		super();
+		this.size = size;
+		this.element = new double[size];
 	}
 		
 	public VectoR(int size, double d) {
@@ -60,8 +60,67 @@ public class VectoR implements Cloneable, java.io.Serializable{
 		}
 	}
 		
-	public int getSize() {
-		return this.size;
+	@Override
+	protected Object clone() throws CloneNotSupportedException {
+		// TODO Auto-generated method stub
+				return super.clone();
+	}
+		
+	public VectoR cross(VectoR v) {
+		try {
+			if(this.size != 3 || v.getSize() != 3)
+				throw new Exception();
+			VectoR result = new VectoR(3);
+			result.setElement(0, this.element[1] * v.getElement(2) - this.element[2] * v.getElement(1));
+			result.setElement(1, this.element[2] * v.getElement(0) - this.element[0] * v.getElement(2));
+			result.setElement(2, this.element[0] * v.getElement(1) - this.element[1] * v.getElement(0));
+			return result;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+		
+	public VectoR div(double d) {
+		return this.mul(1.0 / d);
+	}
+	
+	public double dot(VectoR v) {
+		try {
+			if(this.size != v.getSize())
+				throw new Exception();
+			double result = 0;
+			int i;
+			
+			for(i = 0 ; i < this.size ; i++){
+				result += this.element[i] * v.getElement(i);
+			}
+			
+			return result;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return 0.0;
+		}
+	}
+	
+	@Override
+	public boolean equals(Object obj) {
+		boolean result = false;
+		int i;
+		if (obj instanceof VectoR) {
+			VectoR v = (VectoR) obj;
+
+			if(this.size == v.getSize()) {
+				
+				for(i = 0 ; i < this.size ; i++) {
+					if(this.element[i] != v.getElement(i))
+						return result;
+				}
+				result = true;
+			}
+		}
+
+		return result;
 	}
 		
 	public double[] getElement() {
@@ -73,7 +132,6 @@ public class VectoR implements Cloneable, java.io.Serializable{
 			return element[index];
 			
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 			return 0.0;
 		}
@@ -91,12 +149,22 @@ public class VectoR implements Cloneable, java.io.Serializable{
 				result[i] = this.getElement(i + is);
 			return result;
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 			return null;
 		}
 	}
 	
+	public double[] getElementCopy() {
+		double[] result = new double[this.size];
+		int i;
+		
+		for(i = 0 ; i < this.size ; i++)
+			result[i] = this.element[i];
+		return result;
+	}
+	public int getSize() {
+		return this.size;
+	}
 	public VectoR getSubVector(int is, int ie) {
 
 		VectoR result = new VectoR(ie-is+1);
@@ -106,72 +174,9 @@ public class VectoR implements Cloneable, java.io.Serializable{
 			result = new VectoR(d);
 		}
 		return result;
-	}
-		
-	public void setElement(int index, double d) {
-			
-		try {
-			element[index] = d;
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-		
-	public void setElement(int index, double[] d) {
-		try {
-			int i;
-			
-			if((this.size - index) < d.length) {
-				VectoR vtemp = new VectoR(index);
-				for( i = 0 ; i < index ; i++)
-					vtemp.setElement(i, this.getElement(i));
-				this.element = new double[d.length];
-				this.size = d.length;
-			}			
-			
-			for(i = index ; i < this.size ; i++ )
-				this.element[i] = d[i-index];
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-		}
-	}
-	
-	public void setElement(int index, VectoR v) {
-		setElement(index, v.getElement());
-	}
-	
-	public void setElement(double[] d) {
-		int i;
-		
-		if(this.size < d.length) {
-			this.size = d.length;
-			this.element = new double[d.length];
-		}
-		for(i = 0 ; i < d.length ; i++)
-			this.element[i] = d[i];	
-	}
-	public VectoR plus(VectoR v) {
-		int i;
-		
-		try {
-			if(this.size != v.getSize())
-				 throw new Exception();
-			VectoR result = new VectoR(this.size);
-			
-			for(i = 0 ; i < this.size ; i++)
-				result.setElement(i, this.element[i] + v.getElement(i));
-			
-			return result;
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return null;
-		}
 	}	
 	
-	public VectoR plus(double[] d) {
+	public VectoR minus(double[] d) {
 		int i;
 		try {
 			if(this.size != d.length)
@@ -179,11 +184,10 @@ public class VectoR implements Cloneable, java.io.Serializable{
 			VectoR result = new VectoR(this.size);
 			
 			for(i = 0 ; i < this.size ; i++)
-				result.setElement(i, this.element[i] + d[i]);
+				result.setElement(i, this.element[i] - d[i]);
 			
 			return result;
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 			return null;
 		}	
@@ -202,31 +206,12 @@ public class VectoR implements Cloneable, java.io.Serializable{
 			
 			return result;
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
 			return null;
 		}
 	}	
 	
-	public VectoR minus(double[] d) {
-		int i;
-		try {
-			if(this.size != d.length)
-				throw new Exception();
-			VectoR result = new VectoR(this.size);
-			
-			for(i = 0 ; i < this.size ; i++)
-				result.setElement(i, this.element[i] - d[i]);
-			
-			return result;
-		} catch (Exception e) {
-			// TODO Auto-generated catch block
-			e.printStackTrace();
-			return null;
-		}	
-	}
-	
-	public VectoR mul(double d){
+	public VectoR mul(double d) {
 		VectoR result = new VectoR(this.size);
 		int i;
 		
@@ -236,26 +221,44 @@ public class VectoR implements Cloneable, java.io.Serializable{
 		return result;
 	}
 	
-	public VectoR div(double d) {
-		return this.mul(1.0 / d);
+	public VectoR mul(VectoR d) {
+		try {
+			if(this.size != d.getSize())
+				throw new Exception();
+			VectoR result = new VectoR(this.size);
+			int i;
+			
+			for(i = 0 ; i < this.size ; i++) {
+				result.setElement(i, this.element[i] * d.getElement(i));
+			}
+			return result;
+			
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public double norm() {
+		return norm(2.0);
 	}
 	
 	public double norm(Double r){
 		try {
-			if (r.doubleValue() == 0)
+			if (r.doubleValue() == 0.0)
 				throw new Exception();
 			double result = 0;
 			int i;
 			
 			if(r == Double.POSITIVE_INFINITY) {
-				result = this.element[0];
+				result = Math.abs(this.element[0]);
 				for(i = 1 ; i < this.size ; i++)
-					result = result < this.element[i] ? this.element[i] : result;
+					result = result < Math.abs(this.element[i]) ? Math.abs(this.element[i]) : result;
 			}
 			else if(r == Double.NEGATIVE_INFINITY) {
-				result = this.element[0];
+				result = Math.abs(this.element[0]);
 				for(i = 1 ; i < this.size ; i++)
-					result = result > this.element[i] ? this.element[i] : result;
+					result = result > Math.abs(this.element[i]) ? Math.abs(this.element[i]) : result;
 			}
 			else if(r.intValue() == 1) {
 				for(i = 0 ; i < this.size ; i++)
@@ -263,19 +266,108 @@ public class VectoR implements Cloneable, java.io.Serializable{
 			}
 			else if(r.intValue() == 2) {
 				for(i = 0 ; i < this.size ; i++)
-					result += Math.abs(this.element[i] * this.element[i]);
+					result += this.element[i] * this.element[i];
 				result = Math.sqrt(result);
 			}
 			else {
 				for(i = 0 ; i < this.size ; i++)
 					result += Math.pow(this.element[i], r.doubleValue());
+				result = Math.pow(result, 1.0/r.doubleValue());
 			}
-			result = Math.pow(result, 1.0/r.doubleValue());
 			return result;
 		} catch (Exception e) {
-			// TODO Auto-generated catch block
 			e.printStackTrace();
-			return Double.NaN;
+			return Double.POSITIVE_INFINITY;
 		}		
+	}
+	
+	public VectoR plus(double[] d) {
+		int i;
+		try {
+			if(this.size != d.length)
+				throw new Exception();
+			VectoR result = new VectoR(this.size);
+			
+			for(i = 0 ; i < this.size ; i++)
+				result.setElement(i, this.element[i] + d[i]);
+			
+			return result;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}	
+	}
+	
+	public VectoR plus(VectoR v) {
+		int i;
+		
+		try {
+			if(this.size != v.getSize())
+				 throw new Exception();
+			VectoR result = new VectoR(this.size);
+			
+			for(i = 0 ; i < this.size ; i++)
+				result.setElement(i, this.element[i] + v.getElement(i));
+			
+			return result;
+		} catch (Exception e) {
+			e.printStackTrace();
+			return null;
+		}
+	}
+	
+	public void setElement(double[] d) {
+		int i;
+		
+		if(this.size < d.length) {
+			this.size = d.length;
+			this.element = new double[d.length];
+		}
+		for(i = 0 ; i < d.length ; i++)
+			this.element[i] = d[i];	
+	}
+	
+	public void setElement(int index, double d) {
+			
+		try {
+			element[index] = d;
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void setElement(int index, double[] d) {
+		try {
+			int i;
+			
+			if((this.size - index) < d.length) {
+				VectoR vtemp = new VectoR(index);
+				for( i = 0 ; i < index ; i++)
+					vtemp.setElement(i, this.getElement(i));
+				this.element = new double[d.length];
+				this.size = d.length;
+			}			
+			
+			for(i = index ; i < this.size ; i++ )
+				this.element[i] = d[i-index];
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+	}
+
+	public void setElement(int index, VectoR v) {
+		setElement(index, v.getElement());
+	}
+
+	@Override
+	public String toString() {
+		String str="[\t";
+		int i;
+		for(i = 0 ; i < this.size ; i++) {
+			str += this.element[i];
+			str += "\t";
+		}
+		str += "]";
+		return str;
 	}
 }
