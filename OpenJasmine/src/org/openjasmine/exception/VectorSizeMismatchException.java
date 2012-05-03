5ef39f2701a94f3dc3ee6ad449f1316d203c67b0
elecage <elecage@gmail.com>
2012-05-03 09:00:30 +0000

    PMD 설정 - 중 Exception class 생성

---
+++ OpenJasmine/src/org/openjasmine/exception/VectorSizeMismatchException.java
@@ -0,0 +1,30 @@
+package org.openjasmine.exception;
+
+public class VectorSizeMismatchException extends Exception {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 7465869420847710731L;
+	
+	public VectorSizeMismatchException() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	public VectorSizeMismatchException(String message, Throwable cause) {
+		super(message, cause);
+		// TODO Auto-generated constructor stub
+	}
+
+	public VectorSizeMismatchException(String message) {
+		super(message);
+		// TODO Auto-generated constructor stub
+	}
+
+	public VectorSizeMismatchException(Throwable cause) {
+		super(cause);
+		// TODO Auto-generated constructor stub
+	}
+
+}
