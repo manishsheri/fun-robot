c561b143a9f190162d909f84ab309ff7668c3865
elecage@gmail.com <elecage@gmail.com@6398e2c4-0f80-3df1-c908-689aad24dd59>
2012-05-09 13:04:04 +0000

    Kalman filter test 2개 추가 Geometyr package 추가 RefTran 클래스 추가 - 작성중

---
+++ trunk/OpenJasmine/src/org/openjasmine/geometry/RefTran.java
@@ -0,0 +1,81 @@
+/**
+ * 
+ */
+package org.openjasmine.geometry;
+
+import org.openjasmine.types.Matrix;
+import org.openjasmine.types.VectoR;
+
+/**
+ * @author Hyeon-min.Shim(elecage@gmail.com)
+ *
+ */
+public class RefTran {
+	public static VectoR rotx(double rad, VectoR v) {
+		Matrix rot = new Matrix(3, 3);
+		VectoR res = new VectoR(3);
+		double cosx = Math.cos(rad);
+		double sinx = Math.sin(rad);
+	
+		rot.setElement(0, 0, 1.0);
+		rot.setElement(0, 1, 0.0);
+		rot.setElement(0, 2, 0.0);
+		rot.setElement(1, 0, 0.0);
+		rot.setElement(1, 1, cosx);
+		rot.setElement(1, 2, -sinx);
+		rot.setElement(2, 0, 0.0);
+		rot.setElement(2, 1, sinx);
+		rot.setElement(2, 2, cosx);
+		
+		res = (VectoR)rot.times(v);
+		return res;
+	}
+	public static VectoR rotY(double rad, VectoR v) {
+		Matrix rot = new Matrix(3, 3);
+		VectoR res = new VectoR(3);
+		double cosy = Math.cos(rad);
+		double siny = Math.sin(rad);
+		rot.setElement(0, 0, cosy);
+		rot.setElement(0, 1, 0.0);
+		rot.setElement(0, 2, siny);
+		rot.setElement(1, 0, 0.0);
+		rot.setElement(1, 1, 1.0);
+		rot.setElement(1, 2, 0.0);
+		rot.setElement(2, 0, -siny);
+		rot.setElement(2, 1, 0.0);
+		rot.setElement(2, 2, cosy);
+		
+		res = (VectoR)rot.times(v);
+		return res;
+	}
+	public static VectoR rotZ(double rad, VectoR v) {
+		Matrix rot = new Matrix(3, 3);
+		VectoR res = new VectoR(3);
+		double cosz = Math.cos(rad);
+		double sinz = Math.sin(rad);
+		rot.setElement(0, 0, cosz);
+		rot.setElement(0, 1, -sinz);
+		rot.setElement(0, 2, 0.0);
+		rot.setElement(1, 0, sinz);
+		rot.setElement(1, 1, cosz);
+		rot.setElement(1, 2, 0.0);
+		rot.setElement(2, 0, 0.0);
+		rot.setElement(2, 1, 0.0);
+		rot.setElement(2, 2, 1.0);
+		
+		res = (VectoR)rot.times(v);
+		return res;
+	}
+	
+	public static double d2r(double degree) {
+		double r = degree * Math.PI / 180.0;
+		return r;
+	}
+	
+	public static double r2d(double radian) {
+		double d = radian * 180.0 / Math.PI;
+		
+		return d;
+	}
+	
+}
