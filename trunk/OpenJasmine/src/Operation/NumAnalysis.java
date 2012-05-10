ebe9e09a3f912b30ea92a3a6336594559b294247
elecage@gmail.com <elecage@gmail.com@6398e2c4-0f80-3df1-c908-689aad24dd59>
2012-05-10 13:41:38 +0000

    operation package 생성 LU -> NumAnaly 클래스로 변경

---
+++ trunk/OpenJasmine/src/Operation/NumAnalysis.java
@@ -0,0 +1,11 @@
+package Operation;
+
+import org.openjasmine.types.Matrix;
+import org.openjasmine.types.VectoR;
+
+public class NumAnalysis {	
+	public static VectoR gaussElm(Matrix A, VectoR b) {
+		VectoR result = new VectoR(b.getSize());
+		return result;
+	}
+}
